43c10e2e9f588b4f907f0259d9b3469f77f45ce1
youngrankwon <youngran.kwon@gsitm.com>
2024-01-11 16:46:29 +0900

    프레임워크 버젼 업데이트 & 메뉴관리 ip check 기능 추가

--- node/bo/composables/auth.ts
+++ node/bo/composables/auth.ts
@@ -11,3 +11,19 @@ export const useFunctionAuthCheck = functionId => {
   }
   return authData
 }
+
+export const ipAddrChecker = function () {
+  const ipAddress = $ustra.auth.user.accessInfo.userIp
+  let isAccess = false
+
+  // 공통코드에서 화이트리스트 IP를 조회하여 사용자 IP와 비교
+  // 화이트리스트에 포함되어 있으면 true 로 리턴
+  useUstraCodeList('WHITELIST_IP', true).forEach(v => {
+    if (v.dtlCd.includes(ipAddress)) {
+      isAccess = true
+      return
+    }
+  })
+
+  return isAccess
+}
