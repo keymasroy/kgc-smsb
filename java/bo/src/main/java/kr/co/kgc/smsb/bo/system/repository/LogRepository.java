abfe6e9ffb15eabbcd24dacaecb415236035e43a
youngrankwon <youngran.kwon@gsitm.com>
2024-01-23 16:01:38 +0900

    로그관리 추가

--- java/bo/src/main/java/kr/co/kgc/smsb/bo/system/repository/LogRepository.java
+++ java/bo/src/main/java/kr/co/kgc/smsb/bo/system/repository/LogRepository.java
@@ -1,2 +1,14 @@
-package kr.co.kgc.smsb.bo.system.repository;public class logRepository {
+package kr.co.kgc.smsb.bo.system.repository;
+
+import kr.co.kgc.smsb.bo.system.model.LogModel;
+import kr.co.kgc.smsb.bo.system.model.LogRqVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface LogRepository {
+    List<LogModel> select(String procId, String reqId);
+
+    List<LogModel> selectWithVo(LogRqVo logRqVo);
 }
