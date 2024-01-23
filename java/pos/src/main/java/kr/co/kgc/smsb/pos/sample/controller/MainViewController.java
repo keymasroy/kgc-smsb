300d5069e0fda53801b69e0fdb45336101530305
skw0917 <65753907+skw0917@users.noreply.github.com>
2024-01-23 15:12:16 +0900

    Thymeleaf POST 파라메터 샘플 추가

--- java/pos/src/main/java/kr/co/kgc/smsb/pos/sample/controller/MainViewController.java
+++ java/pos/src/main/java/kr/co/kgc/smsb/pos/sample/controller/MainViewController.java
@@ -1,14 +1,18 @@
 package kr.co.kgc.smsb.pos.sample.controller;
 
-import com.gsitm.ustra.java.management.models.UstraUserModel;
-import kr.co.kgc.smsb.pos.config.SmsbPosConfigProperties;
-import kr.co.kgc.smsb.pos.sample.service.SampleService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import com.gsitm.ustra.java.management.models.UstraUserModel;
+
+import kr.co.kgc.smsb.pos.config.SmsbPosConfigProperties;
+import kr.co.kgc.smsb.pos.sample.service.SampleService;
 
 @Controller
 public class MainViewController {
@@ -35,5 +39,17 @@ public class MainViewController {
         return "sample/list";
     }
 
+    
+    @PostMapping("/post-list")
+    public String postList(Model model, @RequestParam("postValue") String param1) {
+
+        List<UstraUserModel> users = sampleService.getUsers();
+
+        model.addAttribute("prop", smsbPosConfigProperties);
+        model.addAttribute("users", users);
+        model.addAttribute("message", "test message");
+        model.addAttribute("postParam", param1);
+        return "sample/post-list";
+    }
 
 }
